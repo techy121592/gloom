9ebeaec357c211f78f5b85f6778f7afebb814f05
David Welch <techy121592@gmail.com>
2019-04-22 17:12:58 -0400

    Added the license to the top of each source file.

--- Game.h
+++ Game.h
@@ -1,4 +1,21 @@
-// Made by David Welch 2011-2012
+/**
+ * Copyright (C) 2018 David Welch
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; only version 2
+ * of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 /*-------------------------------------------------------------+
  |                     Naming convention                       |
  | local variable       =   first letter lower case            |
@@ -17,8 +34,6 @@
  |      happy sounding music box.                              |
  | The whole atmosphere is a gloomy strange feeling(empty      |
  |      ferris wheel turning in background etc.)               |
- | The artist never gave me what I needed for the art I wanted |
- |      so I have my aweful place holders for now.             |
  +-------------------------------------------------------------*/
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
